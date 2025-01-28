d9166efb8878f39fd64ed7b7a13b41d2bd204e1f
unknown <alieid19961996@gmail.com>
2025-01-28 17:17:03 +0300

    dio cach interceptor

--- lib/core/network/dio_factory.dart
+++ lib/core/network/dio_factory.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:flutter/foundation.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
@@ -37,7 +38,18 @@ class DioFactory {
       receiveTimeout: timeOut,
       headers: headers,
     );
+    final options = CacheOptions(
+      store: MemCacheStore(),
+      policy: CachePolicy.request,
+      hitCacheOnErrorExcept: const [401, 403],
+      maxStale: const Duration(days: 7),
+      priority: CachePriority.high,
+      cipher: null,
+      keyBuilder: CacheOptions.defaultCacheKeyBuilder,
+      allowPostMethod: false,
+    );
     dio.interceptors.add(instance<GeneralInterceptor>());
+    dio.interceptors.add(DioCacheInterceptor(options: options));
     if (kReleaseMode) {
       if (kDebugMode) {
         print('release mode no logs');
